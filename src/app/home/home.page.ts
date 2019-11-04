0f97a7f51ec13c731506741d03a7cce1549d2f21
GuillermoReyesGuerrero <reyesguillermo276@gmail.com>
2019-11-04 11:55:59 -0600

    agregarruta

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { NavController, AlertController, LoadingController } from '@ionic/angular';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,23 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  url="http://localhost:3000";
+  username:string;
+  password:string;
+  datauser:any;
+
+  constructor(public httpc: HttpClient,
+    private router: Router) {
+      this.httpc.get(this.url+"/usuarios")
+        .subscribe(
+            res => {
+                this.datauser = res;
+                console.log(this.datauser);
+            },
+            error => {
+                console.log(error);
+            }
+        );
+    }
 
 }
