5eb1b9a9678d4a39790423742179638aa8c477dc
GuillermoReyesGuerrero <reyesguillermo276@gmail.com>
2019-11-07 22:51:22 -0600

    regsiteruser

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  { path: 'menuadmin', loadChildren: './pages/menuadmin/menuadmin.module#MenuadminPageModule' },
   { path: 'createempresa', loadChildren: './pages/createempresa/createempresa.module#CreateempresaPageModule' },
+  { path: 'registeruser', loadChildren: './pages/registeruser/registeruser.module#RegisteruserPageModule' },
 
 ];
 
